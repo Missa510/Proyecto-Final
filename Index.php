8d37ef3df62b87777aeb405fbb200278d10a367c
Missa510 <sjqv05@gmail.com>
2023-03-23 16:58:16 -0500

    version 0.0.12

--- Index.php
+++ Index.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="Imagenes/Logo.jpg">
-    <link rel="stylesheet" href="Bootstrap/css/Bootstrap.css">
-    <link rel="stylesheet" href="Style/estilo.css">
-    <title>Inversiones Garochoa</title>
-</head>
-<body>
-    <div class="container-fluid">
-        <?php
-            include('Componentes/header.php');
-            include('Componentes/menu.php');
-            include('Componentes/conten.php');
-            include('Componentes/footer.php');
-        ?>
-        <!-- Llamar Bootstrap JS y Bundle JS -->
-        <script src="Bootstrap/js/Bootstrap.bundle.js"></script>
-    </div>
-</body>
-</html>
+<?php
+        include("vista/web/diseno/plantilla.php");
+?>
